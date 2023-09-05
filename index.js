ee3812c8110930be39f4e2b0b98a4dbd2b6b4d79
musabtheexpensive <musabalmahi53@gmail.com>
2023-09-06 00:36:39 +0600

    my another commit here

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const handleCategory = async () => {
-    const response =await fetch(
-        "https://openapi.programming-hero.com/api/videos/categories"
-    );
-    const data =await response.json();
-    const tabContainer=document.getElementById("tab-container");
-    data.data.forEach((category) => {
-        const div =document.createElement("div");
-        div.innerHTML=`
-        <a class="tab">${category.category}</a> 
+  const response = await fetch(
+    "https://openapi.programming-hero.com/api/videos/categories"
+  );
+  const data = await response.json();
+  const tabContainer = document.getElementById("tab-container");
+  data.data.forEach((category) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <a onclick="handleLoadNews('${category.category_id}')" class="tab">${category.category}</a> 
         `;
-        tabContainer.appendChild(div);
-    });
-    console.log(data.data);
+    tabContainer.appendChild(div);
+  });
+  console.log(data.data);
+};
+
+const handleLoadNews =async (categoryId) => {
+  const response=await fetch(
+    `https://openapi.programming-hero.com/api/videos/category/${categoryId}`
+  );
+  const data=await response.json();
+  console.log(data.data);
 };
 handleCategory();
